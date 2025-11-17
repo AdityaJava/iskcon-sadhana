f4862396e1dea2a14b0ab93015de05531f7f7a17
adityadudhal97@gmail.com <adityadudhal97@gmail.com>
2025-11-17 16:18:28 +0530

    Enhance sidebar navigation to load content dynamically for dashboard and reports

--- Main/Main.js
+++ Main/Main.js
@@ -28,9 +28,30 @@ function setupSidebarNavigation() {
 
   options.forEach((option) => {
     option.style.cursor = "pointer"; // show pointer on hover
-    option.addEventListener("click", () => {
+    option.addEventListener("click", async () => {
       const page = option.textContent.trim().toLowerCase(); // e.g., "dashboard" or "reports"
       console.log("Clicked:", page);
+      let pageUrl = "";
+      if (page === "dashboard") {
+        pageUrl = "../SadhanaDashboard/SadhanaDashboard.html";
+      } else if (page === "reports") {
+        pageUrl = "Reports.html";
+      } else {
+        console.warn("No page mapped for:", page);
+        return;
+      }
+      debugger;
+      const contentDiv = document.querySelector("#content");
+      try {
+        const response = await fetch(pageUrl);
+        if (!response.ok) {
+          contentDiv.innerHTML = `<p>Error loading ${page}</p>`;
+        }
+        const html = await response.text();
+        contentDiv.innerHTML = html;
+      } catch (ex) {
+        console.error("Exception:", ex);
+      }
     });
   });
 }
