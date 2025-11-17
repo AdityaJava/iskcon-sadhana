412f45c7a389e45a0a6e78e2e58df354a50b5283
adityadudhal97@gmail.com <adityadudhal97@gmail.com>
2025-11-17 14:50:16 +0530

    Add Dashboard component with HTML, CSS, and JS files

---
+++ Dashboard/Dashboard.js
@@ -0,0 +1,23 @@
+console.log("hey");
+
+async function includeHTML(selector, url) {
+  debugger;
+  const dashBoardElement = document.querySelector(selector);
+  if (!dashBoardElement) return;
+  try {
+    const htmlToInclude = await fetch(url);
+    if (!htmlToInclude.ok) {
+      dashBoardElement.innerHTML = "";
+      return;
+    }
+    const html = await htmlToInclude.text();
+    dashBoardElement.innerHTML = html;
+  } catch (ex) {
+    console.error("Include failed");
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  includeHTML("#header", "../Header/Header.html");
+  includeHTML("#dashboard", "../SideBar/SideBar.html");
+});
